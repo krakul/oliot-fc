1373b45377089c9897de8e9b9244ac6b64e097d9
mikk-leini-krakul <mikk.leini@krakul.eu>
2022-01-06 13:35:22 +0200

    Sensthys SensX EXTREME reader works now. However due to no license, can't distribute the SensX SDK.

--- fc-server/src/main/java/org/fosstrak/ale/server/readers/sensthys/SensXExtremeAdaptor.java
+++ fc-server/src/main/java/org/fosstrak/ale/server/readers/sensthys/SensXExtremeAdaptor.java
@@ -1,5 +1,6 @@
-package org.fosstrak.ale.server.readers.sensx;
+package org.fosstrak.ale.server.readers.sensthys;
 
+import java.math.BigInteger;
 import java.text.SimpleDateFormat;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
@@ -9,8 +10,11 @@ import java.util.List;
 import com.unboundid.util.args.ArgumentException;
 import org.apache.log4j.Logger;
 import org.fosstrak.ale.exception.ImplementationException;
+import org.fosstrak.ale.exception.ValidationException;
 import org.fosstrak.ale.server.Tag;
 import org.fosstrak.ale.server.readers.BaseReader;
+import org.fosstrak.ale.server.readers.LogicalReader;
+import org.fosstrak.ale.server.util.TagHelper;
 import org.fosstrak.ale.xsd.ale.epcglobal.CCOpSpec;
 import org.fosstrak.ale.xsd.ale.epcglobal.CCSpec;
 import org.fosstrak.ale.xsd.ale.epcglobal.LRSpec;
@@ -20,20 +24,22 @@ import org.fosstrak.hal.Observation;
 import RFIDReader.OnTagReadResponse;
 import RFIDReader.RFIDReader;
 import RFIDReader.TagReadInfo;
+import org.fosstrak.tdt.TDTEngine;
 
 /**
  * SensThys SensX EXTREME reader adapter
  */
-public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
+public class SensXExtremeAdaptor extends BaseReader implements OnTagReadResponse {
 
     /** Logger */
-    private static final Logger log = Logger.getLogger(ExtremeAdaptor.class);
+    private static final Logger log = Logger.getLogger(SensXExtremeAdaptor.class);
 
     /** Date & time format which the reader accepts */
-    private static final DateTimeFormatter readerDateTimeFormat = DateTimeFormatter.ofPattern("YYYY-MM-DDThh:mm:ss");
+    private static final DateTimeFormatter readerDateTimeFormat = DateTimeFormatter.ofPattern("YYYY-MM-DD'T'hh:mm:ss");
 
-    /** Reader */
+    /** Variables */
     private RFIDReader reader = null;
+    private TDTEngine tdt = null;
 
     /**
      * initializes a LLRPAdaptor. this method must be called before the Adaptor can
@@ -73,41 +79,61 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
         int port;
         port = Integer.parseInt(portString);
 
-        /* Create reader */
-        log.debug("Create new EXTREME reader, IP " + ip + ", port " + port);
+        /* Create reader - this is just constructor call, which doesn't connect yet */
+        log.debug("Create new SensThys SensX EXTREME reader, IP " + ip + ", port " + port);
         reader = new RFIDReader(ip, port);
+
+        /* Set this class as a read callback handler */
+        reader.SetTagReadCallback(this);
+
+        /* Create TDT engine */
+        tdt = TagHelper.getTDTEngine();
     }
 
+    /**
+     * updates a reader according the specified LRSpec.
+     * @param spec LRSpec for the reader
+     * @throws ImplementationException whenever an internal error occurs
+     */
     @Override
-    public void addTag(Tag tag) {
-        setChanged();
-        notifyObservers(tag);
+    public void update(LRSpec spec) throws ImplementationException {
+
     }
 
+    /**
+     * Triggers the identification of all tags that are currently available
+     * on the reader.
+     * @param readPointNames the readers/sources that have to be polled
+     * @return a set of Observations
+     * @throws HardwareException whenever an internal hardware error occurs (as reader not available...)
+     */
     @Override
-    public void addTags(List<Tag> tags) {
-        setChanged();
-        notifyObservers(tags);
+    public Observation[] identify(String[] readPointNames) throws HardwareException {
+        return new Observation[0];
     }
 
     /**
      * Start reader.
-     * If reader is not yet connected, then connects before start command is sent.
-     * @throws ImplementationException
+     * Connects the reader unless it's not already connected.
      */
     @Override
     public void start() {
+        log.debug("Starting SensThys SensX EXTREME reader...");
 
-        /* Connect reader first */
-        if (isConnected()) {
-            connectReader();
+        /* Already started ? */
+        if (isStarted()) {
+            log.info("SensX is already started");
+            return;
         }
 
-        /* No point of starting if not connected */
-        if (!isConnected())
-        {
-            setStopped();
-            return;
+        /* Connect reader first */
+        if (!isConnected()) {
+            connectReader();
+
+            /* Connection successful ? */
+            if (!isConnected()) {
+                return;
+            }
         }
 
         /* Set UTC date and time of reader */
@@ -121,10 +147,8 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
             log.error("Failed to set date-time of reader: " + e.getMessage());
             e.printStackTrace();
             setStopped();
+            return;
         }
-
-        /* Set this class as a read callback handler */
-        reader.SetTagReadCallback(this);
 
         /* Start continuous read */
         try {
@@ -133,25 +157,24 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
             log.error("Failed to start continuous read: " + e.getMessage());
             e.printStackTrace();
             setStopped();
+            return;
         }
 
         /* Started */
-        log.info("SensX EXTREME reader started");
+        log.info("SensX SensThys EXTREME reader started");
         setStarted();
     }
 
     /**
      * Stop reader.
-     * This is only possible if reader is already connected.
-     * Note: This function does not disconnect reader.
      */
     @Override
     public void stop() {
+        log.debug("Stopping SensThys SensX EXTREME reader...");
 
-        /* Can't stop if not connected */
-        if (!isConnected())
-        {
-            log.error("Can't stop reader if it's not connected.");
+        /* Already stopped ? */
+        if (!isStarted()) {
+            log.info("SensX is already stopped");
             return;
         }
 
@@ -164,29 +187,18 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
         }
 
         /* Stopped */
-        log.info("SensX EXTREME reader stopped");
+        log.info("SensX SensThys EXTREME reader stopped");
         setStopped();
     }
 
     /**
      * Connect reader.
-     * @throws ImplementationException
      */
     @Override
     public void connectReader() {
-
-        /* Can't connect if not initialized */
-        if (reader == null)
-        {
-            log.error("Reader not initialized");
-            setDisconnected();
-            return;
-        }
-
-        log.debug("Connecting SensX EXTREME reader...");
-
-        /* There is no public connect command, we use some read functions to establish and check the connection */
         String hwId, fwId, serial, name;
+
+        /* There is no public "connect" function in SDK, we use some read functions to establish and check the connection */
 
         try {
             hwId = reader.GetHardwareId();
@@ -225,7 +237,7 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
         }
 
         /* Print info */
-        log.info("Connected to SensX EXTREME reader.");
+        log.info("Connected to SensThys SensX EXTREME reader.");
         log.info("  Hardware ID:   " + hwId);
         log.info("  Firmware ID:   " + fwId);
         log.info("  Serial number: " + serial);
@@ -237,38 +249,19 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
 
     /**
      * Disconnect reader.
-     * Note: This function does not stop the reader.
+     * Stops the reader also, if it's started.
      */
     @Override
-    public void disconnectReader() throws ImplementationException {
-
-        /* Can't connect if not initialized */
-        if (reader == null)
-        {
-            log.error("Reader not initialized");
-            setDisconnected();
-            return;
+    public void disconnectReader() {
+        /* Some Fosstrak/Oliot code wants to disconnect reader without stopping it first, so do it now,
+         * otherwise the reader might be in reading mode and won't stop */
+        if (isStarted()) {
+            stop();
         }
-
-        log.debug("Disconnecting SensX EXTREME reader...");
 
         /* Shutdown just closes connection */
         reader.Shutdown();
-
-        /* Print status */
-        log.info("Disconnected from SensX EXTREME reader.");
         setDisconnected();
-    }
-
-    @Override
-    public void update(LRSpec spec) throws ImplementationException {
-        // TODO Implement
-    }
-
-    @Override
-    public Observation[] identify(String[] readPointNames) throws HardwareException {
-        // TODO Implement
-        return new Observation[0];
     }
 
     @Override
@@ -290,15 +283,59 @@ public class ExtremeAdaptor extends BaseReader implements OnTagReadResponse {
      * Tag reading feedback handler.
      * @param readInfo
      */
-    public void handleTagRead(TagReadInfo readInfo)
-    {
+    public void handleTagRead(TagReadInfo readInfo) {
         Tag tag = new Tag();
         tag.setReader(getName());
         tag.setOrigin(getName());
         tag.addTrace("Antenna " + readInfo.antennaNumber);
-        tag.setTagIDAsPureURI(readInfo.EPC); // TODO Not sure it's correct, investigate
         tag.setTimestamp(readInfo.timeStamp.toEpochSecond(ZoneOffset.UTC)); // Get UTC timestamp
 
+        /* Get hexadecimal EPC value.
+         * SensX sends 11-22-33-AA-BB-CC.... first get rid of dashes. */
+        String hex = readInfo.EPC.replace("-", "").toLowerCase();
+        tag.setTagAsHex(hex);
+
+        /* Get binary value */
+        BigInteger bin = new BigInteger(hex, 16);
+        String binString = bin.toString(2);
+        if (binString.startsWith("1") && (binString.length() < 96)) {
+            binString = "00" + binString; /* TODO What if it's still not enough for 96 bits ? */
+        }
+        tag.setTagAsBinary(binString);
+        tag.setTagID(binString.getBytes());
+
+        /* Use conversion to get more data */
+        try {
+            String pureID = TagHelper.convert_to_PURE_IDENTITY(null, null, null, tag.getTagAsBinary());
+            tag.setTagIDAsPureURI(pureID);
+
+            String epc_tag = TagHelper.convert_to_TAG_ENCODING(null, null, null, tag.getTagAsBinary(), tdt);
+            tag.setTagIDAsTagURI(epc_tag);
+
+        } catch (Exception e) {
+            log.debug("Tag decoding error: " + e.getMessage());
+        }
+
         addTag(tag);
+    }
+
+    /**
+     * Report tag to observers.
+     * @param tag Single tags
+     */
+    @Override
+    public void addTag(Tag tag) {
+        setChanged();
+        notifyObservers(tag);
+    }
+
+    /**
+     * Report tags to observers.
+     * @param tags List of tags
+     */
+    @Override
+    public void addTags(List<Tag> tags) {
+        setChanged();
+        notifyObservers(tags);
     }
 }
